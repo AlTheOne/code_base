7b1c9542f6dd77ad3d7db60d5c47ef57753addb7
altheone <altheone.official@gmail.com>
2020-08-30 11:05:12 +0300

    [Add] Competitiveness example

---
+++ competitiveness/example_good.py
@@ -0,0 +1,32 @@
+import random
+import threading
+import time
+
+counter = 0
+count_lock = threading.Lock()
+
+
+def update():
+    global counter
+
+    with count_lock:
+        current_counter = counter
+        time.sleep(random.randint(0, 1))
+        counter = current_counter + 1
+
+
+def main():
+    threads = [threading.Thread(target=update) for i in range(20)]
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    print(f'Final counter: {counter}.')
+    print('Finished.')
+
+
+if __name__ == '__main__':
+    main()
